248c14fcbbe5f8366c7a018ff7facf7f587b40db
yasuo kurihara <breitling1216@gmail.com>
2021-10-26 18:55:47 +0900

    add album code

---
+++ test/fetch_album_test.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:widgets_app/main.dart';
+
+import 'fetch_album_test.mocks.dart';
+
+// MockClient
+@GenerateMocks([http.Client])
+void main() {
+  group('fetchAlbum', () {
+    test('returns an Album if the http call compleates successfully', () async {
+      final client = MockClient();
+
+      when(client
+              .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1')))
+          .thenAnswer((_) async =>
+              http.Response('{"userId": 1, "id": 2, "title": "mock"}', 200));
+
+      expect(await fetchAlbum(client), isA<Album>());
+    });
+
+    test('throws an exception if the http call completes with an error', () {
+      final client = MockClient();
+
+      when(client
+              .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1')))
+          .thenAnswer((_) async => http.Response('Not Found', 404));
+
+      expect(fetchAlbum(client), throwsException);
+    });
+  });
+}
