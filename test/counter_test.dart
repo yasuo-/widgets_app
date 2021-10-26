165f97e5ac887e1794e031ff4aae8a2663946bcf
yasuo kurihara <breitling1216@gmail.com>
2021-10-26 17:55:53 +0900

    add Counter
    - add unit test
    - test package

---
+++ test/counter_test.dart
@@ -0,0 +1,33 @@
+import 'package:test/test.dart';
+import 'package:widgets_app/counter.dart';
+
+void main() {
+  group('Counter', () {
+    test('value should start at 0', () {
+      expect(Counter().value, 0);
+    });
+
+    test("Counter value should be incremented", () {
+      final counter = Counter();
+
+      counter.increment();
+
+      expect(counter.value, 1);
+    });
+  });
+  test("Counter value should be incremented", () {
+    final counter = Counter();
+
+    counter.increment();
+
+    expect(counter.value, 1);
+  });
+
+  test("Counter value should be decremented", () {
+    final counter = Counter();
+
+    counter.decrement();
+
+    expect(counter.value, -1);
+  });
+}
